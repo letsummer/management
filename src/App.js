d6dbb1a79ff73a3783514003e94bed57e13cc76d
letsummer <letsummerparty@gmail.com>
2023-12-24 16:35:23 +0900

    add Customer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
-import './App.css';
+import Customer from "./components/Customer";
+// import './App.css';
+
+const customer = [
+  {
+  'id': 1,
+  'img': 'https://picsum.photos/64',
+  'name': '홍길동',
+  'birth': '961222',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+  'id': 2,
+  'img': 'https://picsum.photos/64',
+  'name': '나동빈',
+  'birth': '961222',
+  'gender': '남자',
+  'job': '강사'
+  },
+  {
+  'id': 3,
+  'img': 'https://picsum.photos/64',
+  'name': '김산타',
+  'birth': '231224',
+  'gender': '여자',
+  'job': '산타'
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {customer.map(c=>
+      <Customer
+        id={c.id}
+        name={c.name}
+        img={c.img}
+        birth={c.birth}
+        gender={c.gender}
+        job={c.job}
+      />)}
     </div>
   );
 }
+
+
+
 
 export default App;
